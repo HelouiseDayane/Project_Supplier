eac8d115186a9661364a5a3cdb3c41d4a63f73d9
helouise <helouisedayane@gmail.com>
2025-03-10 13:37:41 -0300

    update

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,16 @@
 
-flask run --host=0.0.0.0 --port=5000 --reload &
+gunicorn -w 4 -b 0.0.0.0:5000 app:app &
 
 sleep 5
 
-flask db upgrade 
+echo "Esperando o banco de dados estar pronto..."
+until flask db upgrade; do
+  echo "Esperando o banco de dados estar pronto..."
+  sleep 5
+done
+
 
 python3 -m app.database.seeder
+
 
 wait $!
