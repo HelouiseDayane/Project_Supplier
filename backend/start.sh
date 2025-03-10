3d90f95feed50d36908e3442175e2b49a55fb185
helouise <helouisedayane@gmail.com>
2025-03-10 13:49:41 -0300

    teste 3

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
 
+# Ativar o ambiente virtual
 source /app/venv/bin/activate
+
+# Rodar o servidor Flask
 flask run --host=0.0.0.0 --port=5000 --reload &
+
+# Aguardar para garantir que o servidor tenha iniciado
 sleep 5
+
+# Realizar o upgrade do banco de dados
 flask db upgrade
+
+# Rodar o seed do banco de dados
 python3 -m app.database.seeder
+
+# Aguardar até o fim da execução do Flask
 wait $!
